2cdd6950c5468529232114dc58a0d07d9e8961b2
Anders Eriksson <anteman.swe@gmail.com>
2026-04-10 22:27:17 +0200

    refactored code

--- wordCheck.js
+++ wordCheck.js
@@ -1,43 +1,33 @@
-export default function wordCheck (guess, secret) {
-    const guessUpperCase = guess.toUpperCase();
-    const secretUpperCase = secret.toUpperCase();
-
-    let guessSplitted = guessUpperCase.split('');
-    let secretSplitted = secretUpperCase.split('');
-
-    const guessLength = guessSplitted.length;
-
-    let answerArray = new Array(guessLength).fill(null);
-
-    for(let index = 0; index < guessSplitted.length; index++) {
-        if(guessSplitted[index] == secretSplitted[index]) {
+export default function wordCheck(guess, secret) {
+    let guessSplitted = guess.toUpperCase().split('');
+    let secretSplitted = secret.toUpperCase().split('');
+    let answerArray = new Array(guessSplitted.length).fill(null);
+    for (let index = 0; index < guessSplitted.length; index++) {
+        if (guessSplitted[index] == secretSplitted[index]) {
             secretSplitted[index] = null;
-            answerArray[index] = {letter: guessSplitted[index], result: 'correct'};
+            answerArray[index] = { letter: guessSplitted[index], result: 'correct' };
         }
     }
-
-    for(let index = 0; index < guessSplitted.length; index++) {
-        if(!answerArray[index]) {
+    for (let index = 0; index < guessSplitted.length; index++) {
+        if (!answerArray[index]) {
             const letter = guessSplitted[index];
             const findIndex = secretSplitted.indexOf(letter);
-            if(findIndex !== -1){
-                answerArray[index] = {letter: letter, result: 'misplaced'};
+            if (findIndex !== -1) {
+                answerArray[index] = { letter: letter, result: 'misplaced' };
                 secretSplitted[findIndex] = null;
-            } else {
-                answerArray[index] = {letter: letter, result: 'incorrect'};
+            }
+            else {
+                answerArray[index] = { letter: letter, result: 'incorrect' };
             }
         }
     }
-
     return answerArray;
 }
 // Function for checking for difference in length between the words, preparation just in case
 // function checkForDiffLength (guessWord, secretWord) {
 //     let guessFix = guessWord;
 //     let secretFix = secretWord;
-
 //     const anyDiff = guessWord.length - secretWord.length;
-    
 //     if(anyDiff > 0) {
 //         for(let i = 0; i < anyDiff; i++) {
 //             secretFix.push(0); // Using number to not get false positive with null and null
@@ -50,3 +40,4 @@ export default function wordCheck (guess, secret) {
 //     }
 //     return [guessFix, secretFix];
 // }
+//# sourceMappingURL=wordCheck.js.map
